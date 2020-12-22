ab9eaf6e3349b86310724fbc0361f95a5937e8dc
Thomas Geissl <thomas.geissl@gmail.com>
2020-12-22 12:00:44 +0100

    added koesk link and trying to deploy

--- src/components/When.js
+++ src/components/When.js
@@ -8,11 +8,19 @@ export default () => {
         <li>October 29, 2020: Lab30, Augsburg, Germanz</li>
       </ul>
       <ul>
-        <li>October 27, 2019: Köşk, Munich, Germany</li>
+        <li>
+          October 27, 2019:{" "}
+          <a href="https://www.koesk-muenchen.de/" target="_blank">
+            Köşk
+          </a>
+          , Munich, Germany
+        </li>
         <li>
           July 20, 2019:{" "}
-          <a href="https://reservoir.space/">Reservoir Festival</a>,
-          Linachtalsperre, Vöhrenbach, Germany
+          <a href="https://reservoir.space/" target="_blank">
+            Reservoir Festival
+          </a>
+          , Linachtalsperre, Vöhrenbach, Germany
         </li>
         <li>November 17, 2018: Kommandozentrale, Mucca, Munich, Germany</li>
       </ul>
