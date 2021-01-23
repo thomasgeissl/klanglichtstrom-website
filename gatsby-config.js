736696573c7d5e72cd741b928ecbf315cc69e55b
Thomas Geissl <thomas.geissl@gmail.com>
2021-01-23 18:42:46 +0100

    added plugins and changed favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,6 +24,24 @@ module.exports = {
         path: `${__dirname}/src/assets/fonts/`,
       },
     },
+    {
+      resolve: `gatsby-plugin-sharp`,
+      options: {
+        // Available options and their defaults:
+        base64Width: 20,
+        forceBase64Format: ``, // valid formats: png,jpg,webp
+        useMozJpeg: process.env.GATSBY_JPEG_ENCODER === `MOZJPEG`,
+        stripMetadata: true,
+        defaultQuality: 50,
+        failOnError: true,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        icon: `src/assets/favicon.png`, // This path is relative to the root of the site.
+      },
+    },
     `gatsby-plugin-sass`,
   ],
 }
--- src/components/Thomas.js
+++ src/components/Thomas.js
@@ -6,6 +6,12 @@ export default () => {
     <Section>
       <h3>Thomas Geissl</h3>
       TODO
+      {/* <p>
+        Thomas creates experiences that touch people by fusing the analog and
+        the digital world. He believes in free software and enjoys collaborating
+        with others. Music plays an important role in his life. He writes his
+        own performance software and tools.
+      </p> */}
     </Section>
   )
 }
