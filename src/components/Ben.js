b793a0b6c864b58eed090014e3f54d1028eeffda
BenBengler <77937801+BenBengler@users.noreply.github.com>
2021-01-31 13:03:39 +0100

    Update Ben.js

--- src/components/Ben.js
+++ src/components/Ben.js
@@ -5,7 +5,8 @@ export default () => {
   return (
     <Section>
       <h3>Ben Bengler</h3>
-      TODO
+      As musician, performer and installation artist, Ben Bengler is interested in music for the moment, in the power of sound that connects us with the now. He has 
+   played in Germany, UK, Sweden and Austria. His interactive sound installations have been exhibited internationally, including Ars Electronica, Linz, Victoria and Albert Museum (V&ampA), London, Sónar, Barcelona, or Yang Gallery, Beijing.
     </Section>
   )
 }
