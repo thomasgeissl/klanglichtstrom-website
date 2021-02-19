e39761b5d27de8784eb9dba0e6692f6d15b400ce
Thomas Geissl <thomas.geissl@gmail.com>
2021-02-19 09:43:43 +0100

    some minor styling fixes

--- src/components/SubSection.js
+++ src/components/SubSection.js
@@ -4,9 +4,13 @@ import styled from "styled-components"
 const Container = styled.div`
   margin-top: 128px;
   margin-bottom: 196px;
+  padding-left: 25px;
+  padding-right: 25px;
   @media only screen and (max-width: 768px) {
     margin-top: 24px;
     margin-bottom: 32px;
+    padding-left: 0;
+    padding-right: 0;
   }
 `
 
